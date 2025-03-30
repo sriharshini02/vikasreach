6dadf1926c39007f4c9f42d1caf15effcbcedb04
sriharshini02 <sriharshinibhupathiraju@gmail.com>
2025-03-30 19:53:07 +0530

    - updated requirements

--- render-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Install Chrome and ChromeDriver on Render
-echo "Installing Chrome and ChromeDriver..."
-sudo apt-get update
-sudo apt-get install -y chromium-browser
-sudo apt-get install -y chromium-chromedriver
-echo "Chrome and ChromeDriver installation complete."
